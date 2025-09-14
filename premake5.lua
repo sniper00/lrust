7944482a9b203e7f974c2297c45c90abe107746f
bruce <hanyongtao@live.com>
2025-09-14 23:03:52 +0800

    update

--- premake5.lua
+++ premake5.lua
@@ -1,18 +1,21 @@
 ---@diagnostic disable: undefined-global
 local LOCAL_DIR = _SCRIPT_DIR
+if os.host() == "windows" then
+    LOCAL_DIR = path.translate(LOCAL_DIR)
+end
 
 project "lrust"
     kind "Makefile"
 
     filter "configurations:release"
         buildcommands {
-            string.format("{CHDIR} %s;cargo build --release", LOCAL_DIR),
-            string.format("{COPYFILE} %s/target/release/librust.dylib %s ", LOCAL_DIR, MOON_DIR.."/clib/rust.dylib"),
+            string.format("{CHDIR} %s && cargo build --release", LOCAL_DIR),
+            string.format("{COPYFILE} %s/target/release/%s %s ", LOCAL_DIR, get_sharedlib_name("librust"), MOON_DIR.."/clib/"..get_sharedlib_name("rust")),
             string.format("{COPY} %s/lualib/* %s ", LOCAL_DIR, MOON_DIR.."/lualib/ext/"),
         }
     filter "configurations:debug"
         buildcommands {
-            string.format("{CHDIR} %s;cargo build", LOCAL_DIR),
-            string.format("{COPYFILE} %s/target/debug/librust.dylib %s ", LOCAL_DIR, MOON_DIR.."/clib/rust.dylib"),
+            string.format("{CHDIR} %s && cargo build", LOCAL_DIR),
+            string.format("{COPYFILE} %s/target/debug/%s %s ", LOCAL_DIR, get_sharedlib_name("librust"), MOON_DIR.."/clib/"..get_sharedlib_name("rust")),
             string.format("{COPY} %s/lualib/* %s ", LOCAL_DIR, MOON_DIR.."/lualib/ext/"),
         }
