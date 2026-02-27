ddc633e9f8da7653c82b30a1a3bff5c294ac0530
bruce <hanyongtao@live.com>
2026-02-27 22:20:52 +0800

    link

--- crates/libs/lib-lualib/build.rs
+++ crates/libs/lib-lualib/build.rs
@@ -1,12 +1,14 @@
 // extern crate cc;
-// use std::env;
+use std::env;
 // use std::path::PathBuf;
 
 fn main() {
     println!("cargo:rerun-if-changed=lualib-src");
 
     if cfg!(target_os = "windows") {
-        println!(r"cargo:rustc-link-search=native=../../target/release");
+        let profile = env::var("PROFILE").unwrap_or_else(|_| "release".to_string());
+        let profile_dir = if profile == "release" { "release" } else { "debug" };
+        println!(r"cargo:rustc-link-search=native=../../target/{}", profile_dir);
         println!("cargo:rustc-link-lib=dylib=moon");
         println!("cargo:rustc-link-lib=moon");
     } else if cfg!(target_os = "macos") {
